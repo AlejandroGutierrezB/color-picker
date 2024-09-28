28a05d1dddc19ba32d545b17b8b08a2d8ab06161
Alejandro Gutierrez Barcenilla <56408597+AlejandroGutierrezB@users.noreply.github.com>
2024-09-28 10:23:58 +0200

    (setup) install shadcnd  && setup ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ModeToggle } from '@/components/theme/mode-toggle';
+import { ThemeProvider } from '@/components/theme/theme-provider';
+
+const Header = () => {
+  return (
+    <div className="flex flex-row justify-between items-center w-full max-h-12">
+      <h1 className="font-bold text-2xl">Color picker</h1>
+      <ModeToggle />
+    </div>
+  );
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <body className="mx-auto h-screen flex max-w-6xl flex-col items-start justify-center gap-6 p-6 sm:p-10 overflow-hidden">
+        <Header />
+      </body>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
